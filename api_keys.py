4c017019f2a54a55ddd44c1d0f99eea4c2843ddb
Heather <heatherrichardson300@gmail.com>
2024-11-12 18:43:05 -0800

    adding data

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c3a651a08fc17c4d9b4f5d9e93e56a0a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "57223b1b986041f3a3ddd484cf991915"
